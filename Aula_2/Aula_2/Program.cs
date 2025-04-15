d272056bae582b2b4dbc61440e69f5c16ad503b1
willSchnere <schnerwill@gmail.com>
2025-04-14 22:05:08 -0300

    Add files via upload

---
+++ Aula_2/Aula_2/Program.cs
@@ -0,0 +1,28 @@
+﻿string P;
+int Q;
+Double V;
+
+
+Console.WriteLine("Cadastro de Produtos:");
+Console.Write("Nome do Produto:");
+P = Console.ReadLine();
+Console.Write("Quantidade de Produto:");
+String Q0 = Console.ReadLine();
+Q = Convert.ToInt32(Q0);
+Console.Write("Valor do Produto:");
+String V0 = Console.ReadLine();
+V = Convert.ToDouble(V0);
+Console.WriteLine("+----------------------------------------------------------------------------------------------------------------------------------+");
+Console.WriteLine("Produto Digitado");
+Console.WriteLine("Nome Do Produto");
+Console.WriteLine(P);
+Console.WriteLine("Quantidade");
+Console.WriteLine(Q);
+Console.WriteLine("Valor");
+Console.WriteLine(V);
+
+
+
+
+
+
